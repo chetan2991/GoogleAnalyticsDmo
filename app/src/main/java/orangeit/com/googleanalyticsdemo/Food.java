895a9e9faf4381552baec9ab7a7a5ec00834bbaf
sachin <chetanshignare2991@gmail.com>
2016-08-10 09:52:05 +0530

    initial commit

---
+++ app/src/main/java/orangeit/com/googleanalyticsdemo/Food.java
@@ -0,0 +1,32 @@
+package orangeit.com.googleanalyticsdemo;
+
+/**
+ * Created by sachin on 10/8/16.
+ */
+public class Food
+{
+
+    private int id;
+    private String name;
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+}
